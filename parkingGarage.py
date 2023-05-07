456cb0149709ee4ccf0d87cba1a442945f2be5ff
Nicole <melanih428@gmail.com>
2023-05-07 11:20:53 -0400

    message

---
+++ parkingGarage.py
@@ -0,0 +1,22 @@
+# Start Your Code here
+class ticketamount:
+    def __init__(self,ticketamount):
+      self.ticketamount = ticketamount
+def isAvaliable(self):
+    return self.ticket == None
+
+num_tickets = 100
+num_tickets -= 1
+
+print("Number of available tickets: " + str(num_tickets))
+
+class parkingspace:
+    def __init__(self,parkingspace):
+        self.parkingspace = parkingspace
+def IsAvaliable(self):
+        return self.parkingspace == None
+
+num_parkingspace = 100
+num_parkingspace -= 1
+
+print("Number of avaliable parkingspace:" + str(parkingspace))
